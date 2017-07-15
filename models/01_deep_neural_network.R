f88787dd0b41cc83a1d5bcad036306cde9e8a315
NiklasDL <Niklas.Klein@posteo.de>
2017-07-15 16:35:06 +0200

    added preprocessing

---
+++ models/01_deep_neural_network.R
@@ -0,0 +1,14 @@
+################################################################################ 
+################################ Load the data #################################
+################################################################################
+setwd("C:/Users/Niklas/mxnet-tutorials-in-R/data")
+load("train.RData")
+load("test.RData")
+
+train.x = t(data.matrix(trainData))
+train.y = numeric(length = dim(trainLabels)[1])
+
+for(i in 1:9){
+  train.y = train.y + i*trainLabels[, i]
+}
+
