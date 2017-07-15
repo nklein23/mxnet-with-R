1f16340915ce914509da05ede9467efdb86e7b22
NiklasDL <Niklas.Klein@posteo.de>
2017-07-15 19:43:47 +0200

    testing stuff

--- preprocessing/get_mnist.R
+++ preprocessing/get_mnist.R
@@ -1,7 +1,6 @@
 ################################################################################ 
 ############################## Get the mnist data ##############################
 ################################################################################ 
-
 # We will use the darch package as it gives us an easy access to the mnist data.
 # It will download the original files from http://yann.lecun.com/exdb/mnist/ 
 # and convert it into RData files, which can be easily read via load().
