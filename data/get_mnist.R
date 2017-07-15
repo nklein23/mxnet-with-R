7ac251d5f6b84a2ec5fb39242d9ea47497bf0c00
NiklasDL <Niklas.Klein@posteo.de>
2017-07-15 14:50:50 +0200

    added function to get mnist

--- data/get_mnist.R
+++ data/get_mnist.R
@@ -1,1 +1,13 @@
-getData
+################################################################################ 
+################################# Preparations #################################
+################################################################################ 
+# We will use the darch package as it gives us an easy access to the mnist data.
+# It will download the original files from http://yann.lecun.com/exdb/mnist/ 
+# and convert it into RData files, which can be easily read via load().
+require("darch")
+# Set your working directory
+setwd("C:/Users/Niklas/mxnet-tutorials-in-R/data/")
+darch::provideMNIST(folder = "C:/Users/Niklas/mxnet-tutorials-in-R/data/", 
+  download = TRUE)
+
+
