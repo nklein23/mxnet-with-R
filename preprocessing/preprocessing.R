5fdd18c1460bbed783e75982cef3dec5c5b48434
NiklasDL <Niklas.Klein@posteo.de>
2017-07-15 20:52:50 +0200

    small modifications

--- preprocessing/preprocessing.R
+++ preprocessing/preprocessing.R
@@ -9,6 +9,7 @@ setwd("../mxnet-tutorials-in-R/data")
 load("Train.RData")
 load("Test.RData")
 
+## Training data:
 # As you can see, the dimension of the training data are 60000 rows and 784 colums
 dim(trainData)
 # However, we prefer 784 rows and 60000 colums,  which is a very common convention 
@@ -23,7 +24,24 @@ trainLabels_temp = numeric(length = dim(trainLabels)[1])
 for(i in 1:9){
   trainLabels_temp = trainLabels_temp + i*trainLabels[, i]
 }
+rm(i)
 trainLabels = trainLabels_temp
 rm(trainLabels_temp)
-# As we can see, the
+# As we can see, the results match those from the one-hot encoding:
 head(trainLabels)
+
+## Test data:
+testData = t(testData)
+testLabels_temp = numeric(length = dim(testLabels)[1])
+for(i in 1:9){
+  testLabels_temp = testLabels_temp + i*testLabels[, i]
+}
+rm(i)
+testLabels = testLabels_temp
+rm(testLabels_temp)
+
+
+
+
+
+
